1404f4aaf32d4d985878c73fd06f8f0973e31e73
cthacker-udel <cthacker@udel.edu>
2025-11-12 18:05:10 -0500

    chore: added docker compose file and schema init.

---
+++ database_schema.sql
@@ -0,0 +1,23 @@
+-- Creating the ids table.
+CREATE TABLE `ids` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pi_id` varchar(6) DEFAULT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `pi_id` (`pi_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Creating the temperatures table.
+CREATE TABLE `temperatures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `celsius` float NOT NULL,
+  `fahrenheit` float NOT NULL,
+  `kelvin` float NOT NULL,
+  `pi_id` varchar(6) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `fk_pi_id` (`pi_id`),
+  CONSTRAINT `fk_pi_id` FOREIGN KEY (`pi_id`) REFERENCES `ids` (`pi_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
