3b1bb21d5c06e049b73fa1e9b7967ebfbc95fe0c
cthacker-udel <cthacker@udel.edu>
2025-11-18 11:30:04 -0500

    fix: added indexes to the `created_at` columns to ensure performance benefits from continuous queries

--- database_schema.sql
+++ database_schema.sql
@@ -5,7 +5,8 @@ CREATE TABLE `ids` (
   `name` varchar(25) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
-  UNIQUE KEY `pi_id` (`pi_id`)
+  UNIQUE KEY `pi_id` (`pi_id`),
+  KEY `idx_ids_created_at` (`created_at`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Creating the temperatures table.
@@ -18,6 +19,7 @@ CREATE TABLE `temperatures` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   KEY `fk_pi_id` (`pi_id`),
+  KEY `idx_temps_created_at` (`created_at`),
   CONSTRAINT `fk_pi_id` FOREIGN KEY (`pi_id`) REFERENCES `ids` (`pi_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=92 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
